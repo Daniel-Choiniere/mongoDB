c7496e5cff9ff361049b287089c1b177a0f5f014
Daniel Choiniere <danielchoiniere.student@careerdevs.com>
2019-01-22 12:02:23 +0000

    having trouble connecting to mongoDB, going to try and run through terminal instead of c9

--- collection.js
+++ collection.js
@@ -1,6 +1,11 @@
 const mongoose = require('mongoose');
 
 // connect to mongoDB
-mongoose.connect('mongodb://localhost/testaroo');
+mongoose.connect('mongodb:http://careerdevs18-liftedresearch.c9users.io:8080');
 
+mongoose.connection.once('open', function(){
+    console.log('Connection has been made, now make fireworks...');
+}).on('error', function(error){
+    console.log('Connection error:', error);
+});
 
